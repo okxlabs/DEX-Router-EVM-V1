75fecdc62dc64a8084848381be14dec95d3ce5a5
zewei <305670525@qq.com>
2022-03-17 11:15:09 +0800

    add balancer adapter

---
+++ contracts/8/adapter/BalancerAdapter.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../interfaces/IAdapter.sol";
+import "../libraries/UniversalERC20.sol";
+import "../interfaces/IERC20.sol";
+import "../interfaces/IBalancer.sol";
+
+/// @title BalancerAdapter
+/// @notice Explain to an end user what this does
+/// @dev Explain to a developer any extra details
+contract BalancerAdapter is IAdapter {
+    using UniversalERC20 for IERC20;
+
+    function _balancerSwap(address to, address pool, bytes memory moreInfo) internal {
+        (address tokenIn, address tokenOut) = abi.decode(moreInfo, (address, address));
+        address[] memory currentToken = IBalancer(pool).getCurrentTokens();
+
+        bool isTokenInInCurrentToken = false;
+        bool isTokenOutInCurrentToken = false;
+
+        for (uint i=0; i<currentToken.length; i++){
+            if( currentToken[i] == tokenIn){ isTokenInInCurrentToken = true; }
+            if( currentToken[i] == tokenOut){ isTokenOutInCurrentToken = true; }
+        }
+        require(isTokenInInCurrentToken == true, "BalancerAdapter: Wrong FromToken");
+        require(isTokenOutInCurrentToken == true, "BalancerAdapter: Wrong ToToken");
+
+        uint256 tokenAmountIn = IERC20(tokenIn).balanceOf(address(this));
+        SafeERC20.safeApprove(IERC20(tokenIn), pool, tokenAmountIn);
+
+        uint minAmountOut = 0; //Slippage
+        uint maxPrice = 115792089237316195423570985008687907853269984665640564039457584007913129639935; //Slippage
+        
+        IBalancer(pool).swapExactAmountIn(tokenIn, tokenAmountIn, tokenOut, minAmountOut, maxPrice);
+
+        if(to != address(this)) {
+            SafeERC20.safeTransfer(IERC20(tokenOut), to, IERC20(tokenOut).balanceOf(address(this)));
+        }
+    }
+
+    function sellBase(address to, address pool, bytes memory moreInfo) external override {
+        _balancerSwap(to, pool, moreInfo);
+    }
+
+    function sellQuote(address to, address pool, bytes memory moreInfo) external override {
+        _balancerSwap(to, pool, moreInfo);
+    }
+}
