4be579571dfd3ce1df8cdcceb553e062e5cc5907
oker <oker@okerdeMacBook-Pro.local>
2022-03-29 09:54:29 +0800

    test curve meta pool

--- contracts/8/adapter/CurveAdapter.sol
+++ contracts/8/adapter/CurveAdapter.sol
@@ -10,12 +10,17 @@ import "../libraries/SafeERC20.sol";
 contract CurveAdapter is IAdapter {
 
     function _curveSwap(address to, address pool, bytes memory moreInfo) internal {
-        (address fromToken, address toToken, int128 i, int128 j) = abi.decode(moreInfo, (address, address, int128, int128));
+        (address fromToken, address toToken, int128 i, int128 j, bool is_underlying) = abi.decode(moreInfo, (address, address, int128, int128, bool));
         uint256 sellAmount = IERC20(fromToken).balanceOf(address(this));
         // approve
         SafeERC20.safeApprove(IERC20(fromToken),  pool, sellAmount);
+        
         // swap
-        ICurve(pool).exchange(i, j, sellAmount, 0);
+        if (is_underlying) {
+            ICurve(pool).exchange_underlying(i, j, sellAmount, 0);
+        } else {
+            ICurve(pool).exchange(i, j, sellAmount, 0);
+        }
         
         if(to != address(this)) {
             SafeERC20.safeTransfer(IERC20(toToken), to, IERC20(toToken).balanceOf(address(this)));
