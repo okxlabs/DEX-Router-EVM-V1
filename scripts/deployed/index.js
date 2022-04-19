3f224a790edc4a570148b3d0d2066e05ca922560
coder <>
2022-04-19 18:37:19 +0800

    add hardhat-etherscan plugin

--- scripts/deployed/index.js
+++ scripts/deployed/index.js
@@ -1,20 +1,17 @@
 const { network } = require('hardhat');
 
-const oec  = require('./oec')
+const oec  = require('./okc')
 const bsc = require('./bsc')
-const kovan = require('./bsc')
 const eth = require('./eth')
 
 let config
 
 console.log('current network.name: ', network.name)
 
-if (network.name === 'oec') {
+if (network.name === 'okc') {
     config = oec
 } else if (network.name === 'bsc') {
     config = bsc
-} else if (network.name === 'kovan') {
-    config = kovan
 } else if (network.name === 'eth') {
     config = eth
 }
