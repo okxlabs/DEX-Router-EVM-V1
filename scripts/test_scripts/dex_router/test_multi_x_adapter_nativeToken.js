99fea1bd2ae1061812c91f7f0286910afc965aa2
coder <>
2022-05-19 18:03:36 +0800

    optimize test scripts

--- scripts/test_scripts/dex_router/test_multi_x_adapter_nativeToken.js
+++ scripts/test_scripts/dex_router/test_multi_x_adapter_nativeToken.js
@@ -12,17 +12,17 @@ async function main() {
     //       -> USDT CurveV2:50%
     try {
         console.log("\n===========(1 ETH -> USDT Kyber:50%/CurveV2:50%)===========")
-        var account = "0x9199Cc44CF7850FE40081ea6F2b010Fee1088270"
-        var blockNumber = 14436483
-        var fromToken = tokenConfig.tokens.WETH
-        var toToken = tokenConfig.tokens.USDT
-        var amountIn = 1
-        var isFromETH = true;
-        var isToETH = false;
+        let account = "0x9199Cc44CF7850FE40081ea6F2b010Fee1088270"
+        let blockNumber = 14436483
+        let fromToken = tokenConfig.tokens.WETH
+        let toToken = tokenConfig.tokens.USDT
+        let amountIn = 1
+        let isFromETH = true;
+        let isToETH = false;
 
-        var kyberMoreInfo = "0x";
+        let kyberMoreInfo = "0x";
 
-        var curveV2MoreInfo = ethers.utils.defaultAbiCoder.encode(
+        let curveV2MoreInfo = ethers.utils.defaultAbiCoder.encode(
             ["address", "address", "int128", "int128"],
             [
                 fromToken.baseTokenAddress,
@@ -32,13 +32,13 @@ async function main() {
             ]
         )
 
-        var router1 = [
+        let router1 = [
             ["kyber", 5000, tokenConfig.tokens.WETH, tokenConfig.tokens.USDT, kyberUSDTWETHPoolAddress, kyberUSDTWETHPoolAddress, kyberMoreInfo],
             ["curveV2", 5000, tokenConfig.tokens.WETH, tokenConfig.tokens.USDT, curveV2USDTWETHPoolAddress, AssertToSelf, curveV2MoreInfo]
         ];
 
-        var layer1 = [10000,[router1]];
-        var layer = [layer1];
+        let layer1 = [10000,[router1]];
+        let layer = [layer1];
 
 
         await executeMutilXAdapter(account, blockNumber, fromToken, toToken, amountIn, layer, isFromETH, isToETH);
@@ -50,17 +50,17 @@ async function main() {
     //          -> ETH CurveV2:50%
     try {
         console.log("\n===========(1000 USDT -> ETH Kyber:50%/CurveV2:50%)===========")
-        var account = "0x4bfa9e1c47087b0c78783adda7a18ff6461c97b1"
-        var blockNumber = 14660040
-        var fromToken = tokenConfig.tokens.USDT
-        var toToken = tokenConfig.tokens.WETH
-        var amountIn = 1
-        var isFromETH = false;
-        var isToETH = true;
+        let account = "0x4bfa9e1c47087b0c78783adda7a18ff6461c97b1"
+        let blockNumber = 14660040
+        let fromToken = tokenConfig.tokens.USDT
+        let toToken = tokenConfig.tokens.WETH
+        let amountIn = 1
+        let isFromETH = false;
+        let isToETH = true;
 
-        var kyberMoreInfo = "0x";
+        let kyberMoreInfo = "0x";
 
-        var curveV2MoreInfo = ethers.utils.defaultAbiCoder.encode(
+        let curveV2MoreInfo = ethers.utils.defaultAbiCoder.encode(
             ["address", "address", "int128", "int128"],
             [
                 fromToken.baseTokenAddress,
@@ -70,13 +70,13 @@ async function main() {
             ]
         )
 
-        var router1 = [
+        let router1 = [
             ["kyber", 5000, tokenConfig.tokens.USDT, tokenConfig.tokens.WETH, kyberUSDTWETHPoolAddress, kyberUSDTWETHPoolAddress, kyberMoreInfo],
             ["curveV2", 5000, tokenConfig.tokens.USDT, tokenConfig.tokens.WETH, curveV2USDTWETHPoolAddress, AssertToSelf, curveV2MoreInfo]
         ];
 
-        var layer1 = [10000,[router1]];
-        var layer = [layer1];
+        let layer1 = [10000,[router1]];
+        let layer = [layer1];
 
 
         await executeMutilXAdapter(account, blockNumber, fromToken, toToken, amountIn, layer, isFromETH, isToETH);
@@ -89,16 +89,16 @@ async function main() {
     //              ->  bnt  -> usdc (bancor)
     try {
         console.log("\n===========(0.06325 ETH -> USDC uniV2:10%/uniV3:80%/bancor:10%(bnt->usdc))===========")
-        var account = "0x9199Cc44CF7850FE40081ea6F2b010Fee1088270"
-        var blockNumber = 14480567
-        var fromToken = tokenConfig.tokens.WETH
-        var toToken = tokenConfig.tokens.USDC
-        var amountIn = 0.06325
-        var isFromETH = true;
-        var isToETH = false;
+        let account = "0x9199Cc44CF7850FE40081ea6F2b010Fee1088270"
+        let blockNumber = 14480567
+        let fromToken = tokenConfig.tokens.WETH
+        let toToken = tokenConfig.tokens.USDC
+        let amountIn = 0.06325
+        let isFromETH = true;
+        let isToETH = false;
 
-        var uniV2MoreInfo = "0x"
-        var uniV3MoreInfo = ethers.utils.defaultAbiCoder.encode(
+        let uniV2MoreInfo = "0x"
+        let uniV3MoreInfo = ethers.utils.defaultAbiCoder.encode(
             ["uint160", "bytes"],
             [
                 "1353119835187591902566005712305392",
@@ -113,41 +113,40 @@ async function main() {
             ]
         )
 
-        var bancorETHToBNTMoreInfo = ethers.utils.defaultAbiCoder.encode(
+        let bancorETHToBNTMoreInfo = ethers.utils.defaultAbiCoder.encode(
             ["address", "address"],
             [
                 tokenConfig.tokens.ETH.baseTokenAddress,                               // from token address
                 tokenConfig.tokens.BNT.baseTokenAddress                                // to token address
             ]
         )
-        var bancorBNTToUSDCMoreInfo = ethers.utils.defaultAbiCoder.encode(
+        let bancorBNTToUSDCMoreInfo = ethers.utils.defaultAbiCoder.encode(
             ["address", "address"],
             [
-                tokenConfig.tokens.BNT.baseTokenAddress,                               // from token address
+                tokenConfig.tokens.BNT.baseTokenAddress,                                // from token address
                 tokenConfig.tokens.USDC.baseTokenAddress                                // to token address
             ]
         )
 
-        var router1 = [
+        let router1 = [
             ["uniV2", 1000, tokenConfig.tokens.WETH, tokenConfig.tokens.USDC, uniV2USDCWETHPoolAddr, uniV2USDCWETHPoolAddr, uniV2MoreInfo],
             ["uniV3", 8000, tokenConfig.tokens.WETH, tokenConfig.tokens.USDC, uniV3USDCWETHPoolAddr, AssertToSelf, uniV3MoreInfo],
             ["bancor", 1000, tokenConfig.tokens.WETH, tokenConfig.tokens.BNT, bancorETHBNTPoolAddr, AssertToSelf, bancorETHToBNTMoreInfo],
         ];
 
-        var router2 = [
+        let router2 = [
             ["bancor", 10000, tokenConfig.tokens.BNT, tokenConfig.tokens.USDC, bancorBNTUSDCPoolAddr, AssertToSelf, bancorBNTToUSDCMoreInfo],
         ];
 
 
-        var layer1 = [10000,[router1, router2]];
-        var layer = [layer1];
+        let layer1 = [10000,[router1, router2]];
+        let layer = [layer1];
 
         await executeMutilXAdapter(account, blockNumber, fromToken, toToken, amountIn, layer, isFromETH, isToETH);
         
     } catch (error) {
         console.log(error)
     }
-
 }
 
 main()
