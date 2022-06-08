45e362957aaac53a80c48286c5d919df6f592a9f
coder <>
2022-06-08 11:50:40 +0800

    set xbridge address for bsc network

--- scripts/deployed/bsc/base.js
+++ scripts/deployed/bsc/base.js
@@ -4,4 +4,5 @@ module.exports = {
     tokenApprove: "0x2c34A2Fb1d0b4f55de51E1d0bDEfaDDce6b7cDD6",
     tokenApproveProxy: "0xd99cAE3FAC551f6b6Ba7B9f19bDD316951eeEE98",
     dexRouter: "0x9333C74BDd1E118634fE5664ACA7a9710b108Bab",
+    xbridge: '0xFc99f58A8974A4bc36e60E2d490Bb8D72899ee9f'
 }
