fcaf31ff7c773cb6e0cc542a93787d4ffcacf5cf
coder <>
2022-06-09 09:15:07 +0800

    set okc xbridge address

--- scripts/deployed/okc/base.js
+++ scripts/deployed/okc/base.js
@@ -3,5 +3,6 @@ module.exports = {
     wNativeRelayer: "0xd99cAE3FAC551f6b6Ba7B9f19bDD316951eeEE98",
     tokenApprove: "0x70cBb871E8f30Fc8Ce23609E9E0Ea87B6b222F58",
     tokenApproveProxy: "0xE9BBD6eC0c9Ca71d3DcCD1282EE9de4F811E50aF",
-    dexRouter: "0xf6Aab105CB9e66e03CAD2c2F3f8558242593385c"
+    dexRouter: "0xf6Aab105CB9e66e03CAD2c2F3f8558242593385c",
+    xbridge: "0xf956D9FA19656D8e5219fd6fa8bA6cb198094138"
 }
