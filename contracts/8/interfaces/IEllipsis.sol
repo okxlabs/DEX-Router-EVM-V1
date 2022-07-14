0cbe572c1a2fccdc3227906e0669ac1417b8edd3
oker <oker@okerdeMacBook-Pro-98.local>
2022-07-14 11:47:59 +0800

    add Ellipsis

---
+++ contracts/8/interfaces/IEllipsis.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+pragma abicoder v2;
+
+interface IEllipsis {
+    // For two crypto pools
+    function exchange(
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256 minDy
+    ) external payable returns (uint256);
+}
