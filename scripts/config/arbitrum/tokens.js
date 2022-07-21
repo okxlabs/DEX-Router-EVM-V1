00778b5b7f7eca28e9ffd49b7d8c8a9d7db11c74
oker <oker@okerdeMacBook-Pro-98.local>
2022-07-21 13:19:42 +0800

    add synapse

---
+++ scripts/config/arbitrum/tokens.js
@@ -0,0 +1,37 @@
+
+const USDC  = {
+    name: "USDC",
+    decimals: 6,
+    baseTokenAddress: '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8',
+}
+const USDT  = {
+    name: "USDT",
+    decimals: 6,
+    baseTokenAddress: '0x9702230A8Ea53601f5cD2dc00fDBc13d4dF4A8c7',
+}
+
+const nUSD = {
+    name: "nUSD",
+    decimals: 18,
+    baseTokenAddress: '0x2913E812Cf0dcCA30FB28E6Cac3d2DCFF4497688',
+}
+
+const WETH = {
+    name: "WETH",
+    decimals: 18,
+    baseTokenAddress: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
+}
+
+const ETH = {
+    name: "ETH",
+    decimals: 18,
+    baseTokenAddress: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+  }
+
+module.exports = {
+    USDC,
+    USDT,
+    nUSD,
+    WETH,
+    ETH
+}
