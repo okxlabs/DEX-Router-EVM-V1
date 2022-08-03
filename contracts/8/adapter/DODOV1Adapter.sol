3623fafeede438dfbd189d1d8d5aadf34878ca4b
oker <oker@okerdeMacBook-Pro-98.local>
2022-08-03 22:42:44 +0800

    fix dodo v1 and add singel test

--- contracts/8/adapter/DODOV1Adapter.sol
+++ contracts/8/adapter/DODOV1Adapter.sol
@@ -5,6 +5,7 @@ import {IAdapter} from "../interfaces/IAdapter.sol";
 import {IERC20} from "../interfaces/IERC20.sol";
 import "../libraries/SafeERC20.sol";
 import "../interfaces/IDODOV1.sol";
+import "hardhat/console.sol";
 
 contract DODOV1Adapter is IAdapter {
 
@@ -14,10 +15,11 @@ contract DODOV1Adapter is IAdapter {
         _DODO_SELL_HELPER_ = dodoSellHelper;
     }
    
+
     function sellBase(address to, address pool, bytes memory) external override {
         address curBase = IDODOV1(pool)._BASE_TOKEN_();
         uint256 curAmountIn = IERC20(curBase).balanceOf(address(this));
-        SafeERC20.safeTransfer(IERC20(curBase), pool, curAmountIn);
+        SafeERC20.safeApprove(IERC20(curBase), pool, curAmountIn);
         IDODOV1(pool).sellBaseToken(curAmountIn, 0, "");
         if(to != address(this)) {
             address curQuote = IDODOV1(pool)._QUOTE_TOKEN_();
@@ -28,15 +30,24 @@ contract DODOV1Adapter is IAdapter {
     function sellQuote(address to, address pool, bytes memory) external override {
         address curQuote = IDODOV1(pool)._QUOTE_TOKEN_();
         uint256 curAmountIn = IERC20(curQuote).balanceOf(address(this));
-        SafeERC20.safeTransfer(IERC20(curQuote), pool, curAmountIn);
         uint256 canBuyBaseAmount = IDODOSellHelper(_DODO_SELL_HELPER_).querySellQuoteToken(
             pool,
             curAmountIn
         );
+        SafeERC20.safeApprove(IERC20(curQuote), pool, canBuyBaseAmount);
         IDODOV1(pool).buyBaseToken(canBuyBaseAmount, curAmountIn, "");
         if(to != address(this)) {
             address curBase = IDODOV1(pool)._BASE_TOKEN_();
             SafeERC20.safeTransfer(IERC20(curBase), to, canBuyBaseAmount);
+
+            // querySellQuoteToken may cause 2~3 wei token left
+            uint256 curAmountLeft = IERC20(curQuote).balanceOf(address(this));
+            if (curAmountLeft > 0) {
+                SafeERC20.safeTransfer(IERC20(curQuote), to, curAmountLeft);
+            }
         }
+
+
+
     }
 }
---
+++ scripts/test_scripts/single_adapter/test_dodoV1_adapter.js
@@ -0,0 +1,100 @@
+
+
+const { ethers } = require("hardhat");
+require("../../tools");
+const { getConfig } = require("../../config");
+const tokenConfig = getConfig("eth")
+
+async function deployContract() {
+    dodoSellHelper = "0x533dA777aeDCE766CEAe696bf90f8541A4bA80Eb"
+    DODOV1Adapter = await ethers.getContractFactory("DODOV1Adapter");
+    DODOV1Adapter = await DODOV1Adapter.deploy(dodoSellHelper);
+    await DODOV1Adapter.deployed();
+    return DODOV1Adapter
+}
+
+// DODO V1: https://etherscan.io/address/0x75c23271661d9d143dcb617222bc4bec783eff34#readContract
+
+async function sellBaseTesting (DODOV1Adapter) {
+    userAddress = "0x1c11ba15939e1c16ec7ca1678df6160ea2063bc5"
+    therepoolAddress = "0x75c23271661d9d143DCb617222BC4BEc783eff34"
+    startMockAccount([userAddress]);
+
+    signer = await ethers.getSigner(userAddress);
+    
+    // WETH
+    WETH = await ethers.getContractAt(
+        "MockERC20",
+        tokenConfig.tokens.WETH.baseTokenAddress
+    )
+    
+    // USDC
+    USDC = await ethers.getContractAt(
+        "MockERC20",
+        tokenConfig.tokens.USDC.baseTokenAddress
+    )
+
+    // transfer token
+    await WETH.connect(signer).transfer(DODOV1Adapter.address, ethers.utils.parseEther("5"));
+
+    wethBalance = await WETH.balanceOf(DODOV1Adapter.address);
+    usdcBalance = await USDC.balanceOf(DODOV1Adapter.address);
+    console.log("Before WETH Balance: ", wethBalance.toString());
+    console.log("Before USDC Balance: ", usdcBalance.toString());
+
+
+    rxResult = await DODOV1Adapter.sellBase(DODOV1Adapter.address, therepoolAddress, "0x");
+
+    wethBalance = await WETH.balanceOf(DODOV1Adapter.address);
+    usdcBalance = await USDC.balanceOf(DODOV1Adapter.address);
+    console.log("After WETH Balance: ", wethBalance.toString());
+    console.log("After USDC Balance: ", usdcBalance.toString());
+}
+
+async function sellQuoteTesting (DODOV1Adapter) {
+    userAddress = "0x1c11ba15939e1c16ec7ca1678df6160ea2063bc5"
+    therepoolAddress = "0x75c23271661d9d143DCb617222BC4BEc783eff34"
+    startMockAccount([userAddress]);
+
+    signer = await ethers.getSigner(userAddress);
+    
+    // WETH
+    WETH = await ethers.getContractAt(
+        "MockERC20",
+        tokenConfig.tokens.WETH.baseTokenAddress
+    )
+    
+    // USDC
+    USDC = await ethers.getContractAt(
+        "MockERC20",
+        tokenConfig.tokens.USDC.baseTokenAddress
+    )
+
+
+    wethBalance = await WETH.balanceOf(userAddress);
+    usdcBalance = await USDC.balanceOf(userAddress);
+    console.log("Before WETH Balance: ", wethBalance.toString());
+    console.log("Before USDC Balance: ", usdcBalance.toString());
+
+    rxResult = await DODOV1Adapter.sellQuote(userAddress, therepoolAddress, "0x");
+
+    wethBalance = await WETH.balanceOf(userAddress);
+    usdcBalance = await USDC.balanceOf(userAddress);
+    console.log("After WETH Balance: ", wethBalance.toString());
+    console.log("After USDC Balance: ", usdcBalance.toString());
+}
+
+async function main() {  
+    DODOV1Adapter = await deployContract()
+    console.log("======= sellBaseTesting ====== ")
+    await sellBaseTesting(DODOV1Adapter);
+    console.log("======= sellQuoteTesting ====== ")
+    await sellQuoteTesting(DODOV1Adapter);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
