fd169ed16b2bae809c43d865180abf40122c06b6
coder <>
2022-08-10 22:14:56 +0800

    Refine test cases of dodo v1

--- scripts/test_scripts/dex_router/ v2.2.3/test_dodoV1_adapter.js
+++ scripts/test_scripts/dex_router/ v2.2.3/test_dodoV1_adapter.js
@@ -1,14 +1,21 @@
-let { ethers } = require("hardhat");
+const { ethers } = require("hardhat");
 require("../../../tools");
-let { getConfig } = require("../../../config");
+const { getConfig } = require("../../../config");
+const { initDexRouter, direction, FOREVER } = require("../utils")
+
 tokenConfig = getConfig("eth");
-let { initDexRouter, direction, FOREVER } = require("../utils")
 
 async function deployContract() {
-    dodoSellHelper = "0x533dA777aeDCE766CEAe696bf90f8541A4bA80Eb"
-    DODOV1Adapter = await ethers.getContractFactory("DODOV1Adapter");
-    DODOV1Adapter = await DODOV1Adapter.deploy(dodoSellHelper);
-    await DODOV1Adapter.deployed();
+    // dodoSellHelper = "0x533dA777aeDCE766CEAe696bf90f8541A4bA80Eb"
+    // DODOV1Adapter = await ethers.getContractFactory("DODOV1Adapter");
+    // DODOV1Adapter = await DODOV1Adapter.deploy(dodoSellHelper);
+    // await DODOV1Adapter.deployed();
+
+    DODOV1Adapter = await ethers.getContractAt(
+        "DODOV1Adapter",
+        "0x1d0524ad45399FD03f602666f1076C96608EfD28"
+    );
+
     return DODOV1Adapter
 }
 
@@ -90,11 +97,88 @@ async function executeSellBase(DODOV1Adapter) {
     console.log("after Account WETH Balance: " + await WETH.balanceOf(account.address));
 }
 
+async function executeSellQuote(DODOV1Adapter) {
+    const pmmReq = []
+
+    let accountAddress = "0x8894e0a0c962cb723c1976a4421c95949be2d4e3";
+    await startMockAccount([accountAddress]);
+    let account = await ethers.getSigner(accountAddress);
+  
+    // set account balance 0.6 eth
+    await setBalance(accountAddress, "0x53444835ec580000");
+
+    // USDC
+    USDC = await ethers.getContractAt(
+        "MockERC20",
+        tokenConfig.tokens.USDC.baseTokenAddress
+    )
+
+    // WETH
+    WETH = await ethers.getContractAt(
+        "MockERC20",
+        tokenConfig.tokens.WETH.baseTokenAddress
+    )
+
+    console.log("before Account USDC Balance: " + await USDC.balanceOf(account.address));
+    console.log("before Account WETH Balance: " + await WETH.balanceOf(account.address));
+    
+    let fromTokenAmount = ethers.utils.parseUnits("1000", 6);
+
+    let { dexRouter, tokenApprove } = await initDexRouter();
+
+    let minReturnAmount = 0;
+    let deadLine = FOREVER;
+    let poolAddress = "0x75c23271661d9d143DCb617222BC4BEc783eff34"; 
+
+    let mixAdapter1 = [
+        DODOV1Adapter.address
+    ];
+    let assertTo1 = [
+        DODOV1Adapter.address
+    ];
+    let weight1 = Number(10000).toString(16).replace('0x', '');
+    let rawData1 = [
+        "0x" + 
+        "8" + 
+        "0000000000000000000" + 
+        weight1 + 
+        poolAddress.replace("0x", "")  
+    ];
+
+    let moreInfo = "0x";
+    let extraData1 = [moreInfo];
+    let router1 = [mixAdapter1, assertTo1, rawData1, extraData1, USDC.address];
+
+    // layer1
+    // let request1 = [requestParam1];
+    let layer1 = [router1];
+
+    let baseRequest = [
+        USDC.address,
+        WETH.address,
+        fromTokenAmount,
+        minReturnAmount,
+        deadLine,
+    ]
+
+    await USDC.connect(account).approve(tokenApprove.address, fromTokenAmount);
+    await dexRouter.connect(account).smartSwap(
+        baseRequest,
+        [fromTokenAmount],
+        [layer1],
+        pmmReq
+    );
+
+    console.log("after Account USDC  Balance: " + await USDC.balanceOf(account.address));
+    console.log("after Account WETH Balance: " + await WETH.balanceOf(account.address));
+}
 
 async function main() {
-    DODOV1Adapter = await deployContract()
+    DODOV1Adapter = await deployContract();
     console.log(" ============= executeSellBase ===============");
     await executeSellBase(DODOV1Adapter);
+    console.log(" ============= executeSellQuote ===============");
+    await executeSellQuote(DODOV1Adapter);
 }
 
 main()
