201fef2295772abdf0983c2cdacb82b705468b2f
Jason-LI2020 <73786353+Jason-LI2020@users.noreply.github.com>
2022-08-18 17:29:27 +0800

    set feeRate to zero (#62)

--- test_pmm/pmm/pmm_params.js
+++ test_pmm/pmm/pmm_params.js
@@ -1,6 +1,7 @@
 module.exports = {
     feeTo : "0xe35a050360579253881e449d67640d54a9ad8277",
-    feeRate : 30,
+    // feeRate : 30,
+    feeRate : 0,
     backEnd : "0x15d34aaf54267db7d7c367839aaf71a00a2c6a65",     // fifth account of hardhat network
     canceler : "0xc82Ea2afE1Fd1D61C4A12f5CeB3D7000f564F5C6",    // okc test account
     cancelerGuardian : "0xc82Ea2afE1Fd1D61C4A12f5CeB3D7000f564F5C6"     // okc test account
