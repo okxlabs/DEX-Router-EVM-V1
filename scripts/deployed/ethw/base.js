8ba6b8fa10c5ca2e539ca02c23a31082090a6417
coder <zhangxuesong219@gmail.com>
2022-12-19 17:08:42 +0800

    upgrade ethw dexRouter (#164)
    Co-authored-by: coder <>

--- scripts/deployed/ethw/base.js
+++ scripts/deployed/ethw/base.js
@@ -1,6 +1,6 @@
 module.exports = {
     wNativeToken: "0x7Bf88d2c0e32dE92CdaF2D43CcDc23e8Edfd5990",
-    wNativeRelayer: "0x5703B683c7F928b721CA95Da988d73a3299d4757",
+    wNativeRelayer: "0x4D8d82F269A2704867b6cAf58d6e8389e3066eC9",
     tokenApprove: "0x40aA958dd87FC8305b97f2BA922CDdCa374bcD7f",
     tokenApproveProxy: "0x70cBb871E8f30Fc8Ce23609E9E0Ea87B6b222F58",
     dexRouter: "0x51B6812B030a19cF801be8C4B41748488A7e1D60",
