6082ff1189ba9aff8043a43762fdcf94f426585c
oker <oker@okerdeMacBook-Pro-98.local>
2023-03-31 15:11:10 +0800

    set back deployed.base.tokenApproveProxy

--- scripts/09_add_entrance_approve_proxy.js
+++ scripts/09_add_entrance_approve_proxy.js
@@ -6,11 +6,11 @@ async function main() {
 
   const tokenApproveProxy = await ethers.getContractAt(
     "TokenApproveProxy",
-     "0x03B5ACdA01207824cc7Bc21783Ee5aa2B8d1D2fE"
+    deployed.base.tokenApproveProxy
   )
 
-  await tokenApproveProxy.addProxy("0x993e19336003398c1119D9f125936eC9462233F4");
-  console.log(`tokenApproveProxy add proxy 0x993e19336003398c1119D9f125936eC9462233F4`);
+  await tokenApproveProxy.addProxy(deployed.base.investEntrance);
+  console.log(`tokenApproveProxy add proxy ${deployed.base.investEntrance}`);
 }
 
 main()
