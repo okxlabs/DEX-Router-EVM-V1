22c53efb5eab59db5fb03feeebb0cc4b11bae6c2
Pentaclezion <38065267+Pentaclezion@users.noreply.github.com>
2023-05-31 15:54:07 +0800

    stash local (#289)

--- scripts/adapter/pmm_rfq_adapter.js
+++ scripts/adapter/pmm_rfq_adapter.js
@@ -5,7 +5,8 @@ tokenConfig = getConfig("eth");
 async function main() {
 
     //1inch V5: 0x1111111254EEB25477B68fb85Ed929f73A960582
-    const pmmRFQAddr = "0xbcd0fCD2CEF53B927F7FA75F57D08c28862Ed975"; //KRONOS PMM Protocol
+    //const pmmRFQAddr = "0xbcd0fCD2CEF53B927F7FA75F57D08c28862Ed975"; //KRONOS PMM Protocol
+    const pmmRFQAddr = "0xf534a8a1cad0543cd6438f7534ca3486c01998d4"; //KERNEL PMM Protocol
 
     PmmRFQAdapter = await ethers.getContractFactory("PmmRFQAdapter");
     pmmRFQAdapter = await PmmRFQAdapter.deploy(pmmRFQAddr);
