368c30e4aa266447bb8efa89a5f0c3f97a766bda
coder <zhangxuesong219@gmail.com>
2023-07-21 10:53:17 +0800

    deploy dexRouter on linea network (#319)
    Co-authored-by: coder <>

--- scripts/deployed/linea/base.js
+++ scripts/deployed/linea/base.js
@@ -1,9 +1,9 @@
 module.exports = {
     wNativeToken: "0xe5D7C2a44FfDDf6b295A15c148167daaAf5Cf34f",
-    wNativeRelayer: "",
-    tokenApprove: "",
-    tokenApproveProxy: "",
-    dexRouter: "",
+    wNativeRelayer: "0xd2F0aC2012C8433F235c8e5e97F2368197DD06C7",
+    tokenApprove: "0x57df6092665eb6058DE53939612413ff4B09114E",
+    tokenApproveProxy: "0x1b5d39419C268b76Db06DE49e38B010fbFB5e226",
+    dexRouter: "0x6b2C0c7be2048Daa9b5527982C29f48062B34D58",
     xbridge: "",
     nftmarket: "",
     investEntrance: "",
