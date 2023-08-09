ad8ccf9d5339673194296dde9e1b303a7ea13147
coder <zhangxuesong219@gmail.com>
2023-08-09 16:21:06 +0800

    mantle approve proxy earn entrypoint (#338)
    Co-authored-by: coder <>

--- scripts/deployed/mantle/base.js
+++ scripts/deployed/mantle/base.js
@@ -6,7 +6,7 @@ module.exports = {
     dexRouter: "0x6b2C0c7be2048Daa9b5527982C29f48062B34D58",
     xbridge: "",
     nftmarket: "",
-    investEntrance: "",
+    investEntrance: "0x7251FEbEABB01eC9dE53ECe7a96f1C951F886Dd2",
     limitOrder: "",
     protocolAdmin: "0x06C95a3934d94d5ae5bf54731bD2840ceFee6F87",
     _FF_FACTORY: "",
