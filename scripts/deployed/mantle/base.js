0ced451656dd40e284b5e10408dbb77dfd2a131b
coder <zhangxuesong219@gmail.com>
2023-08-18 10:24:25 +0800

    update dexRouter of mantle for fix wmnt error (#341)
    Co-authored-by: coder <>

--- scripts/deployed/mantle/base.js
+++ scripts/deployed/mantle/base.js
@@ -1,6 +1,6 @@
 module.exports = {
     wNativeToken: "0x78c1b0C915c4FAA5FffA6CAbf0219DA63d7f4cb8",
-    wNativeRelayer: "0xd2F0aC2012C8433F235c8e5e97F2368197DD06C7",
+    wNativeRelayer: "0x693EDf0068c24743ad1f36122c8e6D9aCDFD349e",
     tokenApprove: "0x57df6092665eb6058DE53939612413ff4B09114E",
     tokenApproveProxy: "0x1b5d39419C268b76Db06DE49e38B010fbFB5e226",
     dexRouter: "0x6b2C0c7be2048Daa9b5527982C29f48062B34D58",
