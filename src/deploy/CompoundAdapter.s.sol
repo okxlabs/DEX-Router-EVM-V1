afecc499de4064fc0bb23d9eb606b43e0d45a500
bixia <343224563@qq.com>
2023-09-05 17:27:42 +0800

    Usdfi adapter (#348)
    * deploy Usdfi Adapter
    
    ---------
    
    Co-authored-by: Comcat LI <comcatli@Comcats-MacBook-Pro.local>
    Co-authored-by: coder <zhangxuesong219@gmail.com>

--- src/deploy/CompoundAdapter.s.sol
+++ src/deploy/CompoundAdapter.s.sol
@@ -11,15 +11,27 @@ contract Deploy is Test {
 
     function run() public {
         console2.log("deployer", deployer);
-        require(deployer == 0x399EfA78cAcD7784751CD9FBf2523eDf9EFDf6Ad, "wrong deployer! change the private key");
+        require(
+            deployer == 0x358506b4C5c441873AdE429c5A2BE777578E2C6f,
+            "wrong deployer! change the private key"
+        );
 
         // deploy on mainnet
-        vm.createSelectFork(vm.envString("ETH_RPC_URL"));
+        // vm.createSelectFork(vm.envString("ETH_RPC_URL"));
+        // vm.startBroadcast(deployer);
+        // console2.log("block.chainID", block.chainid);
+        // require(block.chainid == 1, "must be mainnet");
+        // address WETH_MAINNET = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+        // address adapter = address(new CompoundAdapter(WETH_MAINNET));
+        // console2.log("CompoundAdapter deployed: %s", adapter);
+        // vm.stopBroadcast();
+
+        vm.createSelectFork(vm.envString("BSC_RPC_URL"));
         vm.startBroadcast(deployer);
         console2.log("block.chainID", block.chainid);
-        require(block.chainid == 1, "must be mainnet");
-        address WETH_MAINNET = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-        address adapter = address(new CompoundAdapter(WETH_MAINNET));
+        require(block.chainid == 56, "must be mainnet");
+        address WBNB = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
+        address adapter = address(new CompoundAdapter(WBNB));
         console2.log("CompoundAdapter deployed: %s", adapter);
         vm.stopBroadcast();
     }
