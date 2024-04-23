99bcb050c55361bfb47fce779628a9bf02bd1d0c
comcat <comc29618@gmail.com>
2024-04-23 17:29:41 +0800

    update

---
+++ scripts/deployed/blast/base.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+    wNativeToken: "0x4300000000000000000000000000000000000004",
+    wNativeRelayer: "0x8853A6Dc8bA7d4D2053f0ad05a23431c30f3a0bE",
+    tokenApprove: "0x5fD2Dc91FF1dE7FF4AEB1CACeF8E9911bAAECa68",
+    tokenApproveProxy: "0xaa6fB20894554d57319c39BCC2468F72af099ea0",
+    dexRouter: "0x2E86f54943faFD2cB62958c3deed36C879e3E944",
+    xbridge: "",
+    //nftmarket: "",
+    investEntrance: "",
+    limitOrder: "",
+    protocolAdmin: "",
+    _FF_FACTORY: "",
+    commisson: "",
+    nftmarketplace: "",
+    //p2pTrading: "",
+    p2pTrading: "",
+    proxyAdmin: "0xF7Dd3E6daB6aaf8a1e853bA00a3e57F60B79527b",
+    newImpl: "",
+}
