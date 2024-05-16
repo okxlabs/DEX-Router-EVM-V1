711107903b1a83c52ef9ce6125ef0940d0c8f097
Lu ZHANG <57679092+LuZHANG1109@users.noreply.github.com>
2024-05-16 17:14:14 +0800

    upgrade dexrouter on polygon (#27)
    Co-authored-by: lu.zhang <lu.zhang@okg.com>
    Co-authored-by: bixia <comcat.li@okg.com>

--- scripts/deployed/polygon/base.js
+++ scripts/deployed/polygon/base.js
@@ -16,4 +16,5 @@ module.exports = {
     // p2pTrading: "0xe6C2820Ba3d14d97b9e9A371771ed7A2f28A9c40",
     p2pTrading: "0x0000000017D9a0231430ff2cc3467375a9Af76C7",
     nftmarketplace: "0xa7FD99748cE527eAdC0bDAc60cba8a4eF4090f7c",
+    newImpl: "0x3a1078eF3B6E36D1560B15f6e0BFa715567A96bA",
 }
