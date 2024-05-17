57072d59a42e043cf9ab0251f577df145fc51bfc
comcat.li <comcat.li@okg.com>
2024-05-17 14:25:52 +0800

    change the common utils

--- contracts/8/libraries/CommonUtils.sol
+++ contracts/8/libraries/CommonUtils.sol
@@ -27,7 +27,7 @@ abstract contract CommonUtils {
   // CRO:     5C7F8A570d578ED84E63fdFA7b1eE72dEae1AE23
   // CFX:     14b2D3bC65e74DAE1030EAFd8ac30c533c976A9b
   // POLYZK   4F9A0e7FD2Bf6067db6994CF12E4495Df938E6e9
-  address public constant _WETH = 0x4300000000000000000000000000000000000004;
+  address public constant _WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
   // address public constant _WETH = 0x5FbDB2315678afecb367f032d93F642f64180aa3;    // hardhat1
   // address public constant _WETH = 0x707531c9999AaeF9232C8FEfBA31FBa4cB78d84a;    // hardhat2
 
@@ -46,7 +46,7 @@ abstract contract CommonUtils {
   // CRO:     E9BBD6eC0c9Ca71d3DcCD1282EE9de4F811E50aF
   // CFX:     100F3f74125C8c724C7C0eE81E4dd5626830dD9a
   // POLYZK   1b5d39419C268b76Db06DE49e38B010fbFB5e226
-  address public constant _APPROVE_PROXY = 0xaa6fB20894554d57319c39BCC2468F72af099ea0;
+  address public constant _APPROVE_PROXY = 0x70cBb871E8f30Fc8Ce23609E9E0Ea87B6b222F58;
   // address public constant _APPROVE_PROXY = 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512;    // hardhat1
   // address public constant _APPROVE_PROXY = 0x2538a10b7fFb1B78c890c870FC152b10be121f04;    // hardhat2
 
@@ -63,7 +63,7 @@ abstract contract CommonUtils {
   // CRO:     40aA958dd87FC8305b97f2BA922CDdCa374bcD7f
   // CFX:     40aA958dd87FC8305b97f2BA922CDdCa374bcD7f
   // POLYZK   d2F0aC2012C8433F235c8e5e97F2368197DD06C7
-  address public constant _WNATIVE_RELAY = 0x8853A6Dc8bA7d4D2053f0ad05a23431c30f3a0bE;
+  address public constant _WNATIVE_RELAY = 0x5703B683c7F928b721CA95Da988d73a3299d4757;
   // address public constant _WNATIVE_RELAY = 0x0B306BF915C4d645ff596e518fAf3F9669b97016;   // hardhat1
   // address public constant _WNATIVE_RELAY = 0x6A47346e722937B60Df7a1149168c0E76DD6520f;   // hardhat2
 
