135c436654f34340e94fc6f19f7e201660704e70
lu.zhang@okg.com <lu.zhang@okg.com>
2024-07-24 16:35:53 +0800

    deploy dexrouter on sei evm

---
+++ scripts/deployed/sei/tokens.js
@@ -0,0 +1,7 @@
+module.exports = {
+    usdt: "0xb75d0b03c06a926e488e2659df1a861f860bd3d1",
+    weth: "0x160345fc359604fc6e70e3c5facbde5f7a9342d8",
+    usdc: "0x3894085ef7ff0f0aedf52e2a2704928d1ec074f1",
+    wsei: "0xe30fedd158a2e3b13e9badaeabafc5516e95e8c7", //18
+    isei: "0x5cf6826140c1c56ff49c808a1a75407cd1df9423" //6
+}
