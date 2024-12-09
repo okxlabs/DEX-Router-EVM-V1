e11188bbbf887e1d31d84f6b77c1cdf17c41ea37
bixia <comcat.li@okg.com>
2024-12-09 14:27:53 +0800

    update (#103)
    Co-authored-by: okxdev <okxdev@gmail.com>

--- src/deploy/NativePmmAdapter.s.sol
+++ src/deploy/NativePmmAdapter.s.sol
@@ -14,15 +14,46 @@ contract Deploy is Test {
         //     deployer == 0xEA522Fe3A65874bA3CEC38e912220282b7C71E97,
         //     "wrong deployer! change the private key"
         // );
-
+        deploy_arb();
+    }
+    function deploy_eth() internal {
         vm.createSelectFork(vm.envString("ETH_RPC_URL"));
-        vm.startBroadcast(deployer);
+
         console2.log("block.chainID", block.chainid);
         require(block.chainid == 1, "must be ethereum");
         address WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+        vm.broadcast(deployer);
         address adapter = address(new NativePmmAdapter(WETH));
         console2.log("NativePmmAdapter deployed ethereum: %s", adapter);
+    }
+    function deploy_bsc() internal {
+        vm.createSelectFork(vm.envString("BSC_RPC_URL"));
 
-        vm.stopBroadcast();
+        console2.log("block.chainID", block.chainid);
+        require(block.chainid == 56, "must be bsc");
+        address WBNB = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
+        vm.broadcast(deployer);
+        address adapter = address(new NativePmmAdapter(WBNB));
+        console2.log("NativePmmAdapter deployed ethereum: %s", adapter);
+    }
+    function deploy_base() internal {
+        vm.createSelectFork(vm.envString("BASE_RPC_URL"));
+
+        console2.log("block.chainID", block.chainid);
+        require(block.chainid == 8453, "must be bsc");
+        address WETH = 0x4200000000000000000000000000000000000006;
+        vm.broadcast(deployer);
+        address adapter = address(new NativePmmAdapter(WETH));
+        console2.log("NativePmmAdapter deployed base: %s", adapter);
+    }
+    function deploy_arb() internal {
+        vm.createSelectFork(vm.envString("ARBI_RPC_URL"));
+
+        console2.log("block.chainID", block.chainid);
+        require(block.chainid == 42161, "must be bsc");
+        address WETH = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
+        vm.broadcast(deployer);
+        address adapter = address(new NativePmmAdapter(WETH));
+        console2.log("NativePmmAdapter deployed arbi: %s", adapter);
     }
 }
