8ba74127dd148fbc684e57d94536980c3324fcf7
okxdex <okxdex@gmail.com>
2025-05-08 11:59:37 +0800

    fix stack too deap for NativeV3Adaptor

--- contracts/8/adapter/NativeV3Adapter.sol
+++ contracts/8/adapter/NativeV3Adapter.sol
@@ -14,7 +14,7 @@ import "../libraries/TickMath.sol";
 /// @title NativeV3Adapter
 /// @notice Only support ERC20 tokens as fromToken and toToken (including WETH)
 /// @dev The fromToken and toToken in moreInfo are underlying tokens, the adapter will deposit underlying token to mint LP tokens
-/// in callback method, then get the LP tokens with swap method, and finally redeem the LP tokens to underlying tokens. 
+/// in callback method, then get the LP tokens with swap method, and finally redeem the LP tokens to underlying tokens.
 contract NativeV3Adapter is IAdapter, INativeV3SwapCallback {
     uint256 internal constant ORIGIN_PAYER =
         0x3ca20afc2ccc0000000000000000000000000000000000000000000000000000;
@@ -39,47 +39,54 @@ contract NativeV3Adapter is IAdapter, INativeV3SwapCallback {
         }
 
         // the fromToken and toToken in data are the underlying tokens
-        (address fromUnderlyingToken, address toUnderlyingToken, uint24 fee) = abi.decode(
-            data,
-            (address, address, uint24)
-        );
+        (
+            address fromUnderlyingToken,
+            address toUnderlyingToken,
+            uint24 fee
+        ) = abi.decode(data, (address, address, uint24));
 
         // get the lpToken of the underlying tokens
-        address fromToken = INativeV3CreditVault(CREDIT_VAULT).lpTokens(fromUnderlyingToken);
-        address toToken = INativeV3CreditVault(CREDIT_VAULT).lpTokens(toUnderlyingToken);
+        address fromToken = INativeV3CreditVault(CREDIT_VAULT).lpTokens(
+            fromUnderlyingToken
+        );
+        address toToken = INativeV3CreditVault(CREDIT_VAULT).lpTokens(
+            toUnderlyingToken
+        );
 
         // get the amount of LP token for a given underlying token balance
-        uint256 sellAmount = INativeLPToken(fromToken).getSharesByUnderlying(IERC20(fromUnderlyingToken).balanceOf(address(this)));
+        uint256 sellAmount = INativeLPToken(fromToken).getSharesByUnderlying(
+            IERC20(fromUnderlyingToken).balanceOf(address(this))
+        );
         bool zeroForOne = fromToken < toToken;
 
-        data = abi.encode(
-            fromToken,
-            toToken,
-            fee
-        );
+        data = abi.encode(fromToken, toToken, fee);
 
         // NativeV3Pool shares the same swap function as UniV3Pool
         IUniV3(pool).swap(
             address(this),
             zeroForOne,
             int256(sellAmount),
-            sqrtX96 == 0
-                ? (
-                    zeroForOne
-                        ? TickMath.MIN_SQRT_RATIO + 1
-                        : TickMath.MAX_SQRT_RATIO - 1
-                )
-                : sqrtX96,
+            zeroForOne
+                ? TickMath.MIN_SQRT_RATIO + 1
+                : TickMath.MAX_SQRT_RATIO - 1,
             data
         );
-        
+
         // redeem toToken to address to
-        INativeLPToken(toToken).redeemTo(INativeLPToken(toToken).sharesOf(address(this)), to);
+        INativeLPToken(toToken).redeemTo(
+            INativeLPToken(toToken).sharesOf(address(this)),
+            to
+        );
 
         // redeem the remaining LP tokens to the payerOrigin
-        uint remainingFromShares = INativeLPToken(fromToken).sharesOf(address(this));
+        uint remainingFromShares = INativeLPToken(fromToken).sharesOf(
+            address(this)
+        );
         if (remainingFromShares > 0 && _payerOrigin != address(0)) {
-            INativeLPToken(fromToken).redeemTo(remainingFromShares, _payerOrigin);
+            INativeLPToken(fromToken).redeemTo(
+                remainingFromShares,
+                _payerOrigin
+            );
         }
     }
 
@@ -129,10 +136,9 @@ contract NativeV3Adapter is IAdapter, INativeV3SwapCallback {
             (address, address, uint24)
         );
 
-        (bool isExactInput, uint256 amountToPay) =
-            amount0Delta > 0
-                ? (tokenIn < tokenOut, uint256(amount0Delta))
-                : (tokenOut < tokenIn, uint256(amount1Delta));
+        (bool isExactInput, uint256 amountToPay) = amount0Delta > 0
+            ? (tokenIn < tokenOut, uint256(amount0Delta))
+            : (tokenOut < tokenIn, uint256(amount1Delta));
         if (isExactInput) {
             pay(tokenIn, address(this), msg.sender, amountToPay);
         } else {
@@ -152,19 +158,22 @@ contract NativeV3Adapter is IAdapter, INativeV3SwapCallback {
         uint256 value
     ) internal {
         address underlying = INativeLPToken(token).underlying();
-        uint256 underlyingAmount = INativeLPToken(token).getUnderlyingByShares(value);
-        // approve LP token contract to transfer underlying token for deposit
-        SafeERC20.safeApprove(
-            IERC20(underlying),
-            token,
-            underlyingAmount
+        uint256 underlyingAmount = INativeLPToken(token).getUnderlyingByShares(
+            value
         );
+        // approve LP token contract to transfer underlying token for deposit
+        SafeERC20.safeApprove(IERC20(underlying), token, underlyingAmount);
         if (payer == address(this)) {
             // deposit owned underlying to mint LP tokens to recipient
             INativeLPToken(token).depositFor(recipient, underlyingAmount);
         } else {
             // transfer underlying from payer to this and deposit to mint LP tokens to recipient
-            SafeERC20.safeTransferFrom(IERC20(underlying), payer, address(this), underlyingAmount);
+            SafeERC20.safeTransferFrom(
+                IERC20(underlying),
+                payer,
+                address(this),
+                underlyingAmount
+            );
             INativeLPToken(token).depositFor(recipient, underlyingAmount);
         }
     }
