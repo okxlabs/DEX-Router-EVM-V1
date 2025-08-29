bf855ccfb7b8203ce3f959e9d3f90fa69af75de0
haodong.wang <haodong.wang@okg.com>
2025-08-29 10:37:48 +0800

    Remove compiler version 0.8.20 of hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -46,16 +46,7 @@ module.exports = {
             runs: 200
           },
         }
-      },
-      {
-        version: '0.8.20',
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200
-          },
-        }
-      },
+      }
     ]
   },
   networks: {
